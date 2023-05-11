0257f85c163b4a53e8363dd89d2eb9390106dcba
Abolfazl <abol@abol.com>
2023-05-11 16:04:49 +0330

    end of lesson 12, end of shooting bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -10,22 +11,23 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
+            (self.settings.screen_width,
+            self.settings.screen_height)
         )
         pygame.display.set_caption("Alien Invasion")
-        
-        self.ship = Ship(self)
-
-        # Set background color
         self.bg_color = (self.settings.bg_color)
 
+        # Objects
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+    
 
     def run_game(self):
         while True :
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
     
     def _check_events(self):
@@ -45,6 +47,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q :
             sys.exit()
+        elif event.key == pygame.K_SPACE :
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT :
@@ -53,9 +57,23 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT :
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_allowed :
+            new_bullet = Bullet(self) 
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0 :
+                self.bullets.remove(bullet)
+
+
     def _update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 
--- justtest.py
+++ justtest.py
@@ -1,2 +1,7 @@
-if True :
-    print('ies')
+l = [ 0,1,2,3]
+
+for i in l.copy() :
+    print(i)
+    l.remove(i)
+    print(l)
+print(l)
